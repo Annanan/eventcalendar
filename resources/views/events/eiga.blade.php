dee3fe7c661fcadc6cb50190736bfd3308d7b94f
anna.hirai <tomato@gmail.com>
2018-07-04 07:18:20 +0000

    eiga and omatsuri.blade.php

---
+++ resources/views/events/eiga.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+ 
+
+@section('content')
+
+   
+        
+        <div class="col-xs-8">
+            <ul class="nav nav-tabs nav-justified">
+                //１行が各ジャンルのタブをあらわす。要ルート変更
+                
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">全イベント <span class="badge">{{ $count_microposts }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">　お祭り・花火大会 <span class="badge">{{ $count_followings }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">映画 <span class="badge">{{ $count_followers }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/favorites') ? 'active' : '' }}"><a href="{{ route('users.favorites', ['id' => $user->id]) }}">レジャー<span class="badge">{{ $count_favorites }}</span></a></li>  
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">スポーツ<span class="badge">{{ $count_microposts }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">音楽<span class="badge">{{ $count_followings }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">グルメ<span class="badge">{{ $count_followers }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/favorites') ? 'active' : '' }}"><a href="{{ route('users.favorites', ['id' => $user->id]) }}">芸術<span class="badge">{{ $count_favorites }}</span></a></li>           
+            </ul>
+
+@foreach ($microposts as $micropost)
+          //  @include(カレンダーのやつ)
+          // ここでは映画のイベントを表示する。
+@endforeach
+           
+        </div>
+    </div>
+
+@endsection
