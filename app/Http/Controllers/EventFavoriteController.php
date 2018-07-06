e05012bacd5ab87ce7dc593e620c5593a9d252de
Shungo.Yanase <shungosp@gmail.com>
2018-07-06 06:17:46 +0000

    edit Event.php and create EventFavoriteController

---
+++ app/Http/Controllers/EventFavoriteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventFavoriteController extends Controller
+{
+    public function store(Request $request, $id)
+    {
+        \Auth::user()->event_favo($id);
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        \Auth::user()->event_unfavo($id);
+        return redirect()->back();
+    }
+}
