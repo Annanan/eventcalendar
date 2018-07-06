7a0411fa863bb4cc787a9ecafd24cdfb2d4e67c3
Shungo.Yanase <shungosp@gmail.com>
2018-07-06 05:51:33 +0000

    add to event favo function User.php by Shungo

--- app/User.php
+++ app/User.php
@@ -26,4 +26,38 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function eventfavorites()
+    {
+        return $this->hasMany(Event::class);
+    }
+    
+    public function event_favo($eventId)
+{
+    $exist = $this->event_favotteru($eventId);
+
+    if ($exist) {
+        return false;
+    } else {
+        $this->eventfavorites()->attach($eventId);
+        return true;
+    }
+}
+
+public function event_unfavo($eventId)
+{
+    $exist = $this->event_favotteru($eventId);
+
+    if ($exist) {
+        $this->eventfavorites()->detach($eventId);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+public function event_favotteru($eventId) {
+    return $this->eventfavorites()->where('event_id', $eventId)->exists();
+}
 }
