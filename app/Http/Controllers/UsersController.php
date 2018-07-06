c84710cba56fd6bd839bb37cb2d7aee1b8687582
Shungo.Yanase <shungosp@gmail.com>
2018-07-06 07:28:20 +0000

    Back to the past

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,4 +9,18 @@ class UsersController extends Controller
     public function mycalendar() {
         return view('mypage.mycalendar');
     }
+    
+    public function event_favorites($id)
+    {
+        $user = User::find($id);
+        $event_favorites = $user->event_favorites()->paginate(50);
+
+        $data = [
+            'event_favorites' => $event_favorites,
+        ];
+
+        // $data += $this->counts($user);
+
+        return view('mypage.mycalendar', $data);
+    }
 }
