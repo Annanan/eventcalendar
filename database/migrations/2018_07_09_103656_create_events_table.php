a37ea0f39312ef032c0de8696ed5eb125360d5be
kanna.hirao <kanna.hirao@rakuten.com>
2018-07-09 02:01:28 +0000

    migration gozen

---
+++ database/migrations/2018_07_09_103656_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('eventdate')->unsigned();
+            $table->string('time');
+            $table->string('prefecture');
+            $table->string('place');
+            $table->string('genre');
+            $table->text('description');
+            $table->string('price');
+            $table->integer('genrenum')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
