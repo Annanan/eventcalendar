f4093c35fc887ab96a41f30855c7e4a854a24f24
daiki.ishikawa <diking@gmail.com>
2018-07-11 06:46:13 +0000

    edited dropdown navbar again

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,25 +14,30 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                     
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span class="gravatar">
-                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
-                                </span>
-                                {{ Auth::user()->name }}
-                                <span class="caret"></span>
-                            </a>
-                            <ul>
-                            <!--<ul class="dropdown-menu">-->
-                                <li>
-                                    <a href="{{ route('mypage.mycalendar', Auth::id()) }}">マイカレンダー</a>
-                                </li>
-                                <!--<li role="separator" class="divider"></li>-->
-                                <li>
-                                    <a href="{{ route('logout.get') }}">ログアウト</a>
-                                </li>
-                            </ul>
-                        </li>
+                    <a href="{{ route('mypage.mycalendar', Auth::id()) }}">マイカレンダー</a>
+                    <a href="{{ route('logout.get') }}">ログアウト</a>
+
+                    
+                        <!--<li class="dropdown">-->
+                        <!--    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
+                        <!--        <span class="gravatar">-->
+                        <!--            <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">-->
+                        <!--        </span>-->
+                        <!--        {{ Auth::user()->name }}-->
+                        <!--        <span class="caret"></span>-->
+                        <!--    </a>-->
+                        <!--    <ul class="dropdown-menu">-->
+                        <!--        <li>-->
+                        <!--            <a href="{{ route('mypage.mycalendar', Auth::id()) }}">マイカレンダー</a>-->
+                        <!--        </li>-->
+                        <!--        <li role="separator" class="divider"></li>-->
+                        <!--        <li>-->
+                        <!--            <a href="{{ route('logout.get') }}">ログアウト</a>-->
+                        <!--        </li>-->
+                        <!--    </ul>-->
+                        <!--</li>-->
+                        
+                        
                     @else
                         <li><a href="{{ route('signup.get') }}">新規登録</a></li>
                         <li><a href="{{ route('login') }}">ログイン</a></li>
