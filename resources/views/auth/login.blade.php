300c957bea43c307ad13726a4b2f03beddfe39eb
Shungo.Yanase <shungosp@gmail.com>
2018-07-13 02:14:28 +0000

    shungo mistake

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,17 +9,34 @@
         <div class="col-md-6 col-md-offset-3">
 
             {!! Form::open(['route' => 'login.post']) !!}
+            <!--<form>-->
+            <!--      <div class="input-group">-->
+            <!--        <span class="input-group-addon">ニックネーム</span>-->
+            <!--        <input type="text" class="form-control" placeholder="ex) Taro">-->
+            <!--      </div>-->
+            <!--</form>-->
+            
+            
                 <div class="form-group">
                     {!! Form::label('name', 'ニックネーム') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
-
+                
+                
+        <!--<br>-->
+        <!--    <form>-->
+        <!--          <div class="input-group">-->
+        <!--            <span class="input-group-addon">パスワード</span>-->
+        <!--            <input type="password" class="form-control" placeholder="6文字以上">-->
+        <!--          </div>-->
+        <!--    </form>-->
+        <!--        <br>-->
                 <div class="form-group">
                     {!! Form::label('password', 'パスワード') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
 
-                {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
 
             <p>New user? {!! link_to_route('signup.get', 'Sign up now!') !!}</p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,22 +9,41 @@
         <div class="col-md-6 col-md-offset-3">
             
             {!! Form::open(['route' => 'signup.post']) !!}
+                <!--<form>-->
+                <!--  <div class="input-group">-->
+                <!--    <span class="input-group-addon">ニックネーム</span>-->
+                <!--    <input type="text" class="form-control" placeholder="ex) Taro">-->
+                <!--  </div>-->
+                <!--</form>-->
                 <div class="form-group">
                     {!! Form::label('name', 'ニックネーム') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
-                
+                <!--<br>-->
+                <!--<form>-->
+                <!--  <div class="input-group">-->
+                <!--    <span class="input-group-addon">パスワード</span>-->
+                <!--    <input type="password" class="form-control" placeholder="6文字以上">-->
+                <!--  </div>-->
+                <!--</form>-->
+                <!--<br>-->
                 <div class="form-group">
                     {!! Form::label('password', 'パスワード') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
-                
+                <!--<form>-->
+                <!--  <div class="input-group">-->
+                <!--    <span class="input-group-addon">パスワード (確認) </span>-->
+                <!--    <input type="password" class="form-control" placeholder="">-->
+                <!--  </div>-->
+                <!--</form>-->
+                <!--<br>-->
                 <div class="form-group">
                     {!! Form::label('password_confirmation', 'パスワード（確認）') !!}
                     {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
                 
-                {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
+                {!! Form::submit('Sign up', ['class' => 'btn btn-warning btn-block']) !!}
             {!! Form::close() !!}
         </div>
     </div>
