d0f13db027158d4fae3e6f6eb1121dfadc59472a
Shungo.Yanase <shungosp@gmail.com>
2018-07-19 06:16:16 +0000

    create and edit cover-style.css

--- resources/views/cover.blade.php
+++ resources/views/cover.blade.php
@@ -10,7 +10,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         
-        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/cover-style.css') }}">
     </head>        
     
     <body>
@@ -21,39 +21,41 @@
     
         <div class=''>
             <div id='wrapper1' class='clearfix'>
-                <figure class='left'>
-                    <img src='suika.jpg' alt='スイカと水うちわ' style='width:300px;height:300px;float:left'>
-                </figure>
+                <div class='left'>
+                    <img src='suika.jpg' alt='スイカと水うちわ' >
+                </div>
                 
-                <figure class='center'>
-                    <img src='furin.jpg' alt='風鈴'style='width:300px;height:300px;float:left'>
-                </figure>
+                <div class='center'>
+                    <img src='furin.jpg' alt='風鈴'>
+                </div>
                 
-                <figure class='right'>
-                    <img src='umi.jpg' alt='海'style='width:300px;height:300px;float:right'>
-                </figure>
+                <div class='right'>
+                    <img src='umi.jpg' alt='海'>
+                </div>
             </div>
             
             
             <div id='wrapper2' class='clearfix'>
-                <figure class='left'>
-                    <img src='hanabi.jpg' alt='お祭り'style='width:300px;height:300px;float:left'>
-                </figure>
+                <div class='left'>
+                    <img src='hanabi.jpg' alt='お祭り'>
+                </div>
                 
-                <figure class='center'>
-                    <img src='fashion.jpg' alt='ファッション'style='width:300px;height:300px;float:left'>
-                </figure>
+                <div class='center'>
+                    <img src='fashion.jpg' alt='ファッション'>
+                </div>
                 
-                <figure class='right'>
-                    <img src='himawari.jpg' alt='ひまわり'style='width:300px;height:300px;float:right'>
-                </figure>
+                <div class='right'>
+                    <img src='himawari.jpg' alt='ひまわり'>
+                </div>
             </div>
             
         </div>
         
-        <div class='登録のところ'>
-            {!! link_to_route('signup.get',  'Sign up', null) !!}
-            {!! link_to_route('login',  'Login', null) !!}
+        <div class='touroku'>
+            <ul>
+                <li class='sign'>{!! link_to_route('login',  'Login', null, ['class'=>'sign']) !!}</li>
+                <li class='sign'>{!! link_to_route('signup.get',  'Sign up', null, ['class'=>'sign']) !!}</li>
+            </ul>
         </div>
 
         
