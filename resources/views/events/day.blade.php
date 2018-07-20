7561020eb9d82d2162fc61f1fea6d600dd8b1772
Shungo.Yanase <shungosp@gmail.com>
2018-07-20 07:16:04 +0000

    edit day.blade

--- resources/views/events/day.blade.php
+++ resources/views/events/day.blade.php
@@ -1,9 +1,17 @@
+
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<div class=zenbu>
+<!--<link rel="stylesheet" href="{{ secure_asset('css/day.css') }}">-->
 
+<h1>8月{{ $date }}日のイベント一覧だお☆</h1>
+<br>
+<br>
+<br>
+<br>
 
-<h1>8月{{ $date }}日のイベント一覧☆</h1>
 <?php
 $omatsuris= DB::table('events')->select('*')->where([['genre', '=','お祭り・花火大会'],['eventdate', '=', $date]])->get();
 $eigas= DB::table('events')->select('*')->where([['genre', '=','映画'],['eventdate', '=', $date]])->get();
@@ -13,11 +21,15 @@ $ongakus= DB::table('events')->select('*')->where([['genre', '=','音楽フェ�
 $geijutsus= DB::table('events')->select('*')->where([['genre', '=','芸術'],['eventdate', '=', $date]])->get();
 $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'],['eventdate', '=', $date]])->get();
 ?>
-<div class="panel panel-info">
+
+<div class="row">
+  <div id=upper>
+  <div class="col-sm-3">
+<div class="panel w3-pink" id=omatsuri>
   <div class="panel-heading">
     <h3 class="panel-title">お祭り・花火大会</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($omatsuris as $event) {  
         echo'('. $event->prefecture .$event->time. ')';
@@ -30,12 +42,14 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     </ul>
   </div>
 </div>
+</div>
 
-<div class="panel panel-info">
+<div class="col-sm-3">
+<div class="panel w3-deep-purple" id=eiga>
   <div class="panel-heading">
     <h3 class="panel-title">映画</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($eigas as $event) {  
         echo'('. $event->prefecture .')';
@@ -48,12 +62,14 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     </ul>
   </div>
 </div>
+</div>
 
-<div class="panel panel-info">
+<div class="col-sm-3">
+<div class="panel w3-cyan" id=sports>
   <div class="panel-heading">
     <h3 class="panel-title">スポーツ</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($sports as $event) {  
         echo'('. $event->prefecture .')';
@@ -66,12 +82,14 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     </ul>
   </div>
 </div>
+</div>
 
-<div class="panel panel-info">
+<div class="col-sm-3">
+<div class="panel w3-lime" id=ongaku>
   <div class="panel-heading">
     <h3 class="panel-title">音楽</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($ongakus as $event) {  
         echo'('. $event->prefecture .')';
@@ -84,13 +102,15 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     </ul>
   </div>
 </div>
+</div>
+</div>
 
-
-<div class="panel panel-info">
+<div class="col-sm-4">
+<div class="panel w3-orange" id=gurume>
   <div class="panel-heading">
     <h3 class="panel-title">グルメ</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($gurumes as $event) {  
         echo'('. $event->prefecture .')';
@@ -103,12 +123,14 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     </ul>
   </div>
 </div>
+</div>
 
-<div class="panel panel-info">
+<div class="col-sm-4">
+<div class="panel w3-deep-orange" id=geijutsu>
   <div class="panel-heading">
     <h3 class="panel-title">芸術</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($geijutsus as $event) {  
         echo'('. $event->prefecture .')';
@@ -121,12 +143,14 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     </ul>
   </div>
 </div>
+</div>
 
-<div class="panel panel-info">
+<div class="col-sm-4">
+<div class="panel w3-brown" id=sonota>
   <div class="panel-heading">
     <h3 class="panel-title">その他</h3>
   </div>
-  <div class="panel-body">
+  <div class="panel-body w3-white">
     <ul class="list-unstyled">
     <?php foreach ($leisures as $event) {  
         echo'('. $event->prefecture .')';
@@ -138,5 +162,9 @@ $leisures= DB::table('events')->select('*')->where([['genre', '=','レジャー'
     ?>
     </ul>
   </div>
+</div>
+</div>
+</div>
+<img src="map.gif" alt="Japan">
 </div>
 @endsection
