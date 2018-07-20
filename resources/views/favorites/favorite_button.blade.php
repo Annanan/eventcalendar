4eb7da8734154215e3c2691fdfa3a70d9c69f2a1
kanna.hirao <kanna.hirao@rakuten.com>
2018-07-20 02:02:34 +0000

    button fix

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,11 +1,11 @@
-
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 @if (Auth::user()->event_favotteru($event->id))
         {!! Form::open(['route' => ['user.unfavoru', $event->id], 'method' => 'delete']) !!}
-            {!! Form::submit('マイカレンダーから外す', ['class' => "btn btn-danger btn-xs", 'style' => "float: left"]) !!}
+            {!! Form::submit('マイカレンダーから外す', ['class' => "w3-blue-grey w3-button w3-wide", 'style' => "float: left"]) !!}
         {!! Form::close() !!}
     @include('favorites.boshu_button')
 @else
         {!! Form::open(['route' => ['user.favoru', $event->id]]) !!}
-            {!! Form::submit('マイカレンダーに追加する', ['class' => "btn btn-primary btn-xs", 'style' => "float: left"]) !!}
+            {!! Form::submit('マイカレンダーに追加する', ['class' => "w3-black w3-button w3-wide", 'style' => "float: left"]) !!}
         {!! Form::close() !!}
 @endif
