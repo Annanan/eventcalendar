998d1d77df8edb1e86212188cce80e7f0daae207
daiki.ishikawa <diking@gmail.com>
2018-07-20 08:03:13 +0000

    added pictures to public folder and changed top-left icon on navbar.blade

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,7 +14,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-left" href="/zenbu"><img src=".jpg" alt="EVENTMOURAKUN"></a>
+                <a class="navbar-left" href="/zenbu"><img src="idea15.jpg" alt="EVENTMOURAKUN" width="390" height="60"></a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
