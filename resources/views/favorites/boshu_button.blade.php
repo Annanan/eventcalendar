1023747c30371e25c7aad5cb48934b8d18dc94cd
hisashi.maedan <ec2-user@ip-172-31-32-120.us-east-2.compute.internal>
2018-07-23 06:17:44 +0000

    modify show page

--- resources/views/favorites/boshu_button.blade.php
+++ resources/views/favorites/boshu_button.blade.php
@@ -3,11 +3,11 @@
 <div class=add>
 @if (Auth::user()->boshuchu($event->id))
         {!! Form::open(['route' => ['user.unboshu', $event->id], 'method' => 'delete']) !!}
-            {!! Form::submit('募集をやめる', ['class' => "w3-grey w3-button w3-wide", 'style' => "float: left"]) !!}
+            {!! Form::submit('募集をやめる', ['class' => "w3-grey w3-button w3-wide", 'style' => "float: left; margin-left: 10px;"]) !!}
         {!! Form::close() !!}
 @else
         {!! Form::open(['route' => ['user.boshu', $event->id]]) !!}
-            {!! Form::submit('募集する', ['class' => "w3-black w3-button w3-wide", 'style' => "float: left"]) !!}
+            {!! Form::submit('募集する', ['class' => "w3-black w3-button w3-wide", 'style' => "float: left; margin-left: 10px;"]) !!}
         {!! Form::close() !!}
 @endif
 </div>
