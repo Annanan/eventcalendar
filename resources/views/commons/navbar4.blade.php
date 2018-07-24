06fcb7dd5ebdb15285a35f303b8f2788a64da0de
hisashi.maedan <ec2-user@ip-172-31-32-120.us-east-2.compute.internal>
2018-07-24 08:09:01 +0000

    modi omatsruri size

--- resources/views/commons/navbar4.blade.php
+++ resources/views/commons/navbar4.blade.php
@@ -13,7 +13,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-left" href="/zenbu"><img src="\idea16.jpg" alt="EVENTMOURAKUN" width="65" height="65"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a class="navbar-left" href="/zenbu"><img src="/idea23.jpg" alt="EVENTMOURAKUN" width="65" height="65"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-center">
