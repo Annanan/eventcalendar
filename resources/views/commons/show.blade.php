33ea62ed3ec6ef917789f2fdc6848b88468a6139
hisashi.maedan <ec2-user@ip-172-31-32-120.us-east-2.compute.internal>
2018-07-24 06:33:19 +0000

    a

--- resources/views/commons/show.blade.php
+++ resources/views/commons/show.blade.php
@@ -8,25 +8,6 @@
 
 
 <div class=eventkuwashiku>
-<<<<<<< HEAD
-<div class="col-xs-12">
-
-    
-  <h2>{{$event->name}}</h2><br>
-  
-  <span class="glyphicon glyphicon-calendar">：{{$event->eventdate}}日</span><br><br><br>
-  <span class="glyphicon glyphicon-time">：{{$event->time}}</span><br><br><br>
-  <span class="glyphicon glyphicon-map-marker">：{{$event->prefecture}} {{$event->place}}</span><br><br><br>
-  <span class="glyphicon glyphicon-list-alt">：{{$event->description}}</span><br><br><br>
-  <span class="glyphicon glyphicon-usd">：{{$event->price}}</span><br><br><br>
-  <span class="glyphicon glyphicon-book">：{{$event->genre}}</span><br><br><br>
-  
-</div>
-
-@if(Auth::check())
-@include('favorites.favorite_button', ['event' => $event])
-<li class="w3-gray w3-button w3-wide"><a href="{{ route('mypage.mycalendar', Auth::id()) }}">マイカレンダーを見る</a></li> 
-=======
  <div class=description>
   <div class="col-xs-12">
   
@@ -48,7 +29,6 @@
 @if(Auth::check())
 @include('favorites.favorite_button', ['event' => $event])
 <li class="w3-black w3-button w3-wide"><a href="{{ route('mypage.mycalendar', Auth::id()) }}">マイカレンダーを見る</a></li>
->>>>>>> ebf25eeb3e84cd7f41e68da7f07768953d8cfa98
 </div>
 
 <div class='panel panel-info scroll'>
@@ -57,21 +37,7 @@
  <span class="glyphicon glyphicon-user"> {{$user->name}} ({{$user->group}})</span><br><br>
  @endforeach
 </div>
-
-
-
-<<<<<<< HEAD
-<h3>募集しているユーザー一覧</h3><br>
-@foreach($users as $user)
-<span class="glyphicon glyphicon-user">{{$user->name}} ({{$user->group}})</span><br><br>
-@endforeach
-=======
->>>>>>> ebf25eeb3e84cd7f41e68da7f07768953d8cfa98
-</div>
-
 @endif
-
-
 
 </body>
 @endsection
