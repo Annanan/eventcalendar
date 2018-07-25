d1a94c5d32ad7394d833632426e663da2044cbc3
daiki.ishikawa <diking@gmail.com>
2018-07-25 04:27:18 +0000

    changed login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">       <title>Eventcalendar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">       <title>EVENT MOURAKUN</title>
         
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -24,7 +24,7 @@
       
       
       <div class=setsumei>
-        <h1 class=copy>作ろう、自分だけのイベントカレンダー。</h1>
+        <h1 class=copy><br>作ろう、自分だけのイベントカレンダー。</h1>
 
          <h3 class= bun> このアプリの使い方<br>HOW TO USE THIS APP</h3>
        <div class=picture id=setsumei1><p class=bunbun id=bunbun1>①:カレンダーからイベントを検索</p>
