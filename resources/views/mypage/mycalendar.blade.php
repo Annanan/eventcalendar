faecaa0a311a70533f87215a4095deeb7cb0b7d3
hisashi.maedan <ec2-user@ip-172-31-32-120.us-east-2.compute.internal>
2018-07-25 04:38:36 +0000

    login placeholder

--- resources/views/mypage/mycalendar.blade.php
+++ resources/views/mypage/mycalendar.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 @section('content')
 <br>
-<p style="font-size:25px">[{{ $user->name }}さんのマイカレンダー]</p>　<p class = 'boshu1'> ※募集中のイベントは背景がピンクです</p> 
+<p style="font-size:25px">[{{ $user->name }}さんのマイカレンダー]</p>　<p class = 'boshu1'> ※{{ $user->name }}さんが募集しているイベントは背景がピンクです</p> 
 
         <div class="col-xs-12">
             @include('commons.mypagecalendar', ['events' => $event_favorites, 'user' => $user ])
