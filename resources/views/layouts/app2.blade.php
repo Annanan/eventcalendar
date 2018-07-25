29686eac20ccb300932f1098db9809a6ff105f2b
kanna.hirao <kanna.hirao@rakuten.com>
2018-07-25 03:19:35 +0000

    heroku navbar not working test

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -12,6 +12,13 @@
         
         <link rel="stylesheet" href="{{ secure_asset('css/all.css') }}">
         
+        <!-- Bootstrap first -->
+<link rel="stylesheet" media="all" href="/assets/base/bootstrap.css"  type="text/css">
+
+<!-- Your extra styling -->
+<link rel="stylesheet" media="all" href="/assets/application-52c2922bf3888faae91327ac966f10f93474a159ef20a14180eb9bd164ed87cf.css" data-turbolinks-track="true" />
+
+        
     </head>
 
     <body>
